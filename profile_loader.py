4ab9fff7d5454adf80b25b78e0ec5cf423a43da3
Nikita Churikov <nikita@chur.ru>
2018-11-28 13:41:19 +0300

    Add logging

--- profile_loader.py
+++ profile_loader.py
@@ -2,6 +2,7 @@ import os
 import time
 import json
 from random import randint
+import logging
 
 from PIL import Image
 import requests
@@ -11,6 +12,21 @@ import tinder_api
 import fb_auth_token
 from config import *
 
+logger = logging.getLogger('tinder_scraper')
+logger.setLevel(logging.DEBUG)
+
+fh_logger = logging.FileHandler('scraper_logs.log')
+fh_logger.setLevel(logging.DEBUG)
+
+sh_logger = logging.StreamHandler()
+sh_logger.setLevel(logging.ERROR)
+
+logging_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh_logger.setFormatter(logging_format)
+sh_logger.setFormatter(logging_format)
+
+logger.addHandler(fh_logger)
+logger.addHandler(sh_logger)
 
 class TinderScraper:
 
@@ -28,12 +44,20 @@ class TinderScraper:
         fb_access_token = fb_auth_token.get_fb_access_token(self.__fb_login, self.__fb_password)
         fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
 
-        tinder_api.get_auth_token(fb_access_token, fb_user_id)
+        tinder_token = tinder_api.get_auth_token(fb_access_token, fb_user_id)
+        if tinder_token:
+            logging.info('Login to Tinder is successful')
+        else:
+            logging.error('Login to Tinder was not successful')
 
     def save_profiles(self):
         recommendations = tinder_api.get_recommendations()
+        n_new_profiles = 0
+        n_new_photos = 0
+        logging.info(f'Got {len(recommendations["results"])} recommendations')
         for res in recommendations['results']:
             if res['_id'] not in self.ids:
+                n_new_profiles += 1
                 self.ids.add(res['_id'])
                 path = os.path.join(self.save_path, str(res['_id']))
                 os.mkdir(path)
@@ -45,6 +69,8 @@ class TinderScraper:
                     response = requests.get(url)
                     img = Image.open(BytesIO(response.content))
                     img.save(os.path.join(path, img_id + '.jpg'))
+                    n_new_photos += 1
+        logging.info(f'Got {n_new_profiles} new profiles and {n_new_photos} new photos.')
 
 
 if __name__ == '__main__':
@@ -54,4 +80,5 @@ if __name__ == '__main__':
     while True:
         tin_scraper.save_profiles()
         n_sec = randint(120, 320)
+        logging.info(f'Sleep for {n_sec} seconds')
         time.sleep(n_sec)

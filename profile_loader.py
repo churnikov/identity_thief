d9ca231d5474734fa6eb8951afe20af246e9894f
Nikita Churikov <nikita@chur.ru>
2019-05-10 16:21:18 +0300

    UPD: Tinder is now used as library NEW: now using mongodb as storage NEW: requirements

--- profile_loader.py
+++ profile_loader.py
@@ -1,85 +1,112 @@
-import os
-import time
-import json
-from random import randint
 import logging
 
-from PIL import Image
-import requests
-from io import BytesIO
-
-import tinder_api
 import fb_auth_token
-from config import *
+import requests
+import tinder_api
+from bson import Binary
+from dependencies import Injector, operation
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
 
-logger = logging.getLogger('tinder_scraper')
-logger.setLevel(logging.DEBUG)
+from config import fb_username, fb_password
 
-fh_logger = logging.FileHandler('scraper_logs.log')
-fh_logger.setLevel(logging.DEBUG)
 
-sh_logger = logging.StreamHandler()
-sh_logger.setLevel(logging.ERROR)
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
-logging_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh_logger.setFormatter(logging_format)
-sh_logger.setFormatter(logging_format)
+    fh_logger = logging.FileHandler("scraper_logs.log")
+    fh_logger.setLevel(logging.DEBUG)
 
-logger.addHandler(fh_logger)
-logger.addHandler(sh_logger)
+    sh_logger = logging.StreamHandler()
+    sh_logger.setLevel(logging.DEBUG)
 
-class TinderScraper:
+    logging_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    fh_logger.setFormatter(logging_format)
+    sh_logger.setFormatter(logging_format)
 
-    def __init__(self, fb_login, fb_password, save_path=None):
-        self.save_path = save_path or 'Tinder/'
-        if not os.path.exists(self.save_path):
-            os.mkdir(self.save_path)
-        self.ids = set(os.listdir(self.save_path))
+    logger.addHandler(fh_logger)
+    logger.addHandler(sh_logger)
 
+    return logger
+
+
+def get_database(host, port, db_name) -> Database:
+    client = MongoClient(host, port)
+    db = client[db_name]
+    return db
+
+
+class TinderConnector:
+    def __init__(self, fb_login: str, fb_password: str, logger: logging.Logger) -> None:
+        self.logger = logger
         self.__fb_login = fb_login
         self.__fb_password = fb_password
 
-    def init(self):
-
+    def connect(self):
         fb_access_token = fb_auth_token.get_fb_access_token(self.__fb_login, self.__fb_password)
         fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
 
         tinder_token = tinder_api.get_auth_token(fb_access_token, fb_user_id)
         if tinder_token:
-            logging.info('Login to Tinder is successful')
+            self.logger.info("Login to Tinder is successful")
         else:
-            logging.error('Login to Tinder was not successful')
+            self.logger.error("Login to Tinder was not successful")
+
+
+class TinderSaver:
+    def __init__(self, db: Database, logger: logging.Logger, collection_name: str = "profiles"):
+
+        self.logger = logger
+        self.collection_name = collection_name
+        self.db = db
+        self.collection: Collection = db[self.collection_name]
 
     def save_profiles(self):
         recommendations = tinder_api.get_recommendations()
         n_new_profiles = 0
         n_new_photos = 0
-        logging.info(f'Got {len(recommendations["results"])} recommendations')
-        for res in recommendations['results']:
-            if res['_id'] not in self.ids:
-                n_new_profiles += 1
-                self.ids.add(res['_id'])
-                path = os.path.join(self.save_path, str(res['_id']))
-                os.mkdir(path)
-                with open(os.path.join(path, 'info.json'), 'w') as f:
-                    json.dump(res, f)
-                for p in res['photos']:
-                    url = p['processedFiles'][0]['url']
-                    img_id = p['id']
-                    response = requests.get(url)
-                    img = Image.open(BytesIO(response.content))
-                    img.save(os.path.join(path, img_id + '.jpg'))
-                    n_new_photos += 1
-        logging.info(f'Got {n_new_profiles} new profiles and {n_new_photos} new photos.')
+        self.logger.info(f"Got {len(recommendations['results'])} recommendations")
+        for res in filter(
+            lambda r: self.collection.find_one(filter={"_id": r["_id"]}) is None,
+            recommendations["results"],
+        ):
+            profile = {
+                "distance_mi": res["distance_mi"],
+                "_id": res["_id"],
+                "bio": res["bio"],
+                "birth_date": res["birth_date"],
+                "name": res["name"],
+                "photos": [],
+            }
+            n_new_profiles += 1
+
+            for p in res["photos"]:
+                url = p["url"]
+                response = requests.get(url)
+                img = Binary(response.content)
+                profile["photos"].append(
+                    {"img": img, "filename": p["fileName"], "extension": p["extension"], "url": url}
+                )
+                n_new_photos += 1
+
+        self.logger.info(f"Got {n_new_profiles} new profiles and {n_new_photos} new photos.")
 
 
-if __name__ == '__main__':
-    with open('config.json', 'r') as f:
-        save_path = json.load(f)['tinder_scraper']['save_path']
-    tin_scraper = TinderScraper(fb_username, fb_password, save_path=save_path)
-    tin_scraper.init()
-    while True:
-        tin_scraper.save_profiles()
-        n_sec = randint(120, 320)
-        logging.info(f'Sleep for {n_sec} seconds')
-        time.sleep(n_sec)
+class Loader(Injector):
+    @operation
+    def load(saver, connector):
+        connector.connect()
+        saver.save_profiles()
+    saver = TinderSaver
+    collection_name = 'profiles'
+    db = get_database(host = None, port = None, db_name = 'tinder')
+    connector = TinderConnector
+    fb_login = fb_username
+    fb_password = fb_password
+    logger = get_logger()
+
+
+if __name__ == "__main__":
+    Loader.load()
